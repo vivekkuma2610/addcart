0ccb2310b31e2527bc37a812b4b64b83d149e610
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-12-19 17:00:31 +0530

    update

--- src/components/home.js
+++ src/components/home.js
@@ -4,8 +4,8 @@ export function Home(){
         <header className="bg-dark py-5">
     <div className="container px-4 px-lg-5 my-5">
         <div className="text-center text-white">
-            <h1 className="display-4 fw-bolder">Welcome to vivek kumar shopping world</h1>
-            <p className="lead fw-normal text-white-50 mb-0">get updated with technology</p>
+            <h1 className="display-4 fw-bolder">Welcome to vivek kumar shopping portal</h1>
+            <p className="lead fw-normal text-white-50 mb-0">we are your style statemnt</p>
         </div>
     </div>
 </header>
