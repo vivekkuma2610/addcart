d98a028829e3f6612f4f50f8b9e51d31f858ec20
vivekkuma2610 <vivekpanneer2610@gmail.com>
2023-12-19 15:14:15 +0530

    update

--- src/components/home.js
+++ src/components/home.js
@@ -5,7 +5,7 @@ export function Home(){
     <div className="container px-4 px-lg-5 my-5">
         <div className="text-center text-white">
             <h1 className="display-4 fw-bolder">Welcome to vivek kumar shopping world</h1>
-            <p className="lead fw-normal text-white-50 mb-0">we meet all your style statements</p>
+            <p className="lead fw-normal text-white-50 mb-0">get updated with technology</p>
         </div>
     </div>
 </header>
